977d8a9311a9539553169b17742cea94881dae0b
Tom <tkeevins@cox.net>
2023-01-15 07:21:04 -0700

    added Seach component and layout

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,12 +2,15 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
+const navbarStyle = {
+    backgroundColor: 'lightblue'
+}
 
 const Header = ({title}) => {
     
     return(
-        <Navbar bg="light" expand="lg">
-        <Container fluid>
+        <Navbar style={navbarStyle} expand="lg">
+        <Container>
           <Navbar.Brand href="/">{title}</Navbar.Brand>
         </Container>
       </Navbar>
---
+++ frontend/src/components/Search.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+const Search = () => {
+  return (
+    <Container className="mt-4">
+      <Row className="justify-content-center">
+        <Col xs={12} md={8} lg={6}>
+          <Form>
+            <Row>
+              <Col xs={9}>
+                <Form.Control placeholder="Search for new image..." />
+              </Col>
+              <Col>
+                <Button variant="primary" type="submit">Search</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Search;

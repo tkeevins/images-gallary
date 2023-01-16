2522aea3d5f6144081c5616eec92a6075e4fdaa6
Tom <tkeevins@cox.net>
2023-01-15 19:01:53 -0700

    first API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Search from "./components/Search";
 import { useState } from "react";
 
-
+const UNSPLASH_KEY = process.env.REACT_APP_UNSPLASH_KEY;
 
 function App() {
   const [word, setWord] = useState("");
@@ -11,7 +11,16 @@ function App() {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
+    fetch(`https://api.unsplash.com/photos/random/?query=${word}&client_id=${UNSPLASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
   };
+
 
   
   return (
